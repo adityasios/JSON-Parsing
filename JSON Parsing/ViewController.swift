e743031a539ce1be13fda7b90461e52c3ac608cb
Aditya Kumar <webmaazix@gmail.com>
2019-01-25 18:30:47 +0530

    Initial Commit

---
+++ JSON Parsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON Parsing
+//
+//  Created by Rakhi on 25/01/19.
+//  Copyright © 2019 myself. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
