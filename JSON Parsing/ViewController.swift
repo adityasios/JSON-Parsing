09cd62c7d0c24b9cacfa3b6f0f84123389a6c167
Aditya Kumar <webmaazix@gmail.com>
2019-01-25 19:05:27 +0530

    Json Parsing Initial

--- JSON Parsing/ViewController.swift
+++ JSON Parsing/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos") else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let dataResponse = data , error == nil else {
+                print(error?.localizedDescription ?? "Response Error")
+                return
+            }
+            
+            do{
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                    dataResponse, options: [])
+                print(jsonResponse)
+                self.parseJson(json: jsonResponse)
+            } catch let parsingError {
+                print("Error", parsingError)
+            }
+        }
+        task.resume()
     }
-
-
+    
+    
+    func parseJson(json:Any){
+        guard let jsonArray = json as? [[String: Any]] else {
+            return
+        }
+        
+        guard let title = jsonArray[0]["title"] as? String else { return }
+        print(title)
+        
+    }
 }
 
